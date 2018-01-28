04faa4fa583286349d6b826a11bee98b589106a7
ArtiomTapunov <Tapunov_AT@tut.by>
2018-01-28 14:26:09 +0300

    Added new project. Gitignore updated

---
+++ SN.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SN.WEB.Startup))]
+namespace SN.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
