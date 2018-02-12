d8515b76317f1668772ae47f9067066e818dde9f
Nick137 <proga1379@mail.ru>
2018-02-12 15:59:31 +0300

    Side bar

---
+++ SN.WEB/Areas/Friends/Controllers/FriendController.cs
@@ -0,0 +1,27 @@
+﻿using SN.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SN.WEB.Areas.Friends.Controllers
+{
+    public class FriendController : Controller
+    {
+
+        private readonly IUserService _userService;
+
+        public FriendController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        // GET: Friends/Friend
+        public ActionResult Index()
+        {
+            return View(_userService.GetAllFriends());
+        }
+    }
+}
