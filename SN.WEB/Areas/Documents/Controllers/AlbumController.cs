d200bf701e59a6297b2b92f79f652354837ae7d0
Nick137 <proga1379@mail.ru>
2018-02-14 14:39:46 +0300

    deleting angular

---
+++ SN.WEB/Areas/Documents/Controllers/AlbumController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SN.WEB.Areas.Documents.Controllers
+{
+    public class AlbumController : Controller
+    {
+
+        public AlbumController()
+        {
+
+        }
+
+
+        // GET: Documents/Album
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
