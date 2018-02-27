262f62ba38f5d6774d095aa70d9a60b66a147a01
Nick137 <proga1379@mail.ru>
2018-02-27 22:37:02 +0300

    Some commit

---
+++ SN.DomainModels/SocialNetworkIModels/Community.cs
@@ -0,0 +1,14 @@
+﻿using SN.DomainModels.AbstratClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SN.DomainModels.SocialNetworkIModels
+{
+    public class Community : Group
+    {
+        public ICollection<Post> Content { get; set; }
+    }
+}
---
+++ SN.DomainModels/SocialNetworkIModels/GroupChat.cs
@@ -0,0 +1,14 @@
+﻿using SN.DomainModels.AbstratClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SN.DomainModels.SocialNetworkIModels
+{
+    public class GroupChat : Group
+    {
+        public ICollection<Message> Сorrespondence { get; set; }
+    }
+}
